c5848627147b8dae1348889dc47f99f1d3b73dec
Gyuhub <pmg6484@naver.com>
2021-12-15 14:22:10 +0900

    Add tf broadcaster

--- include/roswrapper.h
+++ include/roswrapper.h
@@ -1,6 +1,11 @@
 #pragma once
+// ROS header ///////////////////////////////
 #include <ros/ros.h>
+// ROS message header ///////////////////////
 #include <std_msgs/Float32MultiArray.h>
+#include <shm_ros/srvwrapper.h>
+#include <tf/transform_broadcaster.h>
+/////////////////////////////////////////////
 #include <string.h>
 #include "shm.h"
 
@@ -12,15 +17,36 @@ class ROSWrapper
 public:
     ROSWrapper(const NodeHandle& nh, Shm* shm);
     ~ROSWrapper();
-    Publisher _pub;
-    Subscriber _sub;
-    NodeHandle _nh;
 
     void *internalThreadRoutine();
     static void* externalThreadRoutine(void* arg) {return ((ROSWrapper *)arg)->internalThreadRoutine();};
 private:
     Shm& _shm;
-    
-    void initialize();
+// ROS Node & Service objects ///////////////
+    NodeHandle _nh;
+    Publisher _pub;
+    Subscriber _sub;
+    ServiceServer _serv;
+/////////////////////////////////////////////
+
+// ROS variables ////////////////////////////
+    tf::TransformBroadcaster _tf_br;
+    tf::Transform _tf_transform;
+    tf::Vector3 _tf_vec;
+    tf::Quaternion _tf_quat;
+/////////////////////////////////////////////
+
+// ROS Custom functions /////////////////////
+    void broadcastTF();
+/////////////////////////////////////////////
+
+// ROS CallBack functions ///////////////////
     // void SubscribeCallBack(const std_msgs msg);
+    bool serviceCallBack(shm_ros::srvwrapper::Request& req, shm_ros::srvwrapper::Response& res);
+/////////////////////////////////////////////
+
+// ROS wrapper initialization ///////////////
+    void ROSInitialize();
+    void initialize();
+/////////////////////////////////////////////
 };
--- src/roswrapper.cpp
+++ src/roswrapper.cpp
@@ -22,10 +22,40 @@ void* ROSWrapper::internalThreadRoutine()
     return NULL;
 }
 
+void ROSWrapper::broadcastTF()
+{
+    for (int i = 0; i < 3; i++) _tf_vec[i] = _shm._shared_memory->_x(i);
+    _tf_transform.setOrigin(_tf_vec);
+    double roll  = _shm._shared_memory->_x(3);
+    double pitch = _shm._shared_memory->_x(4);
+    double yaw   = _shm._shared_memory->_x(5);
+    _tf_quat.setRPY(roll, pitch, yaw);
+    _tf_transform.setRotation(_tf_quat);
+    _tf_br.sendTransform(tf::StampedTransform(_tf_transform, ros::Time::now(), "base_frame", "end_effector"));
+}
+
 // void ROSWrapper::SubscribeCallBack(const std_msgs msg)
 // {
 // }
 
+bool ROSWrapper::serviceCallBack(shm_ros::srvwrapper::Request& req, shm_ros::srvwrapper::Response& res)
+{
+    // Do what you want!
+    double x = req.x;
+    double y = req.y;
+    double result = x + y;
+    res.result = result;
+}
+
+void ROSWrapper::ROSInitialize()
+{
+    // _pub = _nh.advertise<//Some ROS standard message type ex) std_msgs::String...>("/Topic name", Queue size(normaly '1'));
+    // _sub = _nh.subscribe<//Some ROS standard message type ex) std_msgs::String...>("/Topic", Queue size (normaly 1), &ROSWrapper::SomeCallBackFunction, this);
+    _serv = _nh.advertiseService("/Service_name", &ROSWrapper::serviceCallBack, this);
+    ROS_INFO("* Initialize ROS settings!");
+}
+
 void ROSWrapper::initialize()
 {
+    ROSInitialize();
 }
